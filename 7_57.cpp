aa52ee2dd72a07c26768b719a40aef9472451018
Aaricis <947165603@qq.com>
2020-11-13 21:59:40 +0800

    update

---
+++ 7_57.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+class Account{
+public:
+    void calculate(){
+        amount += amount*interestRate;
+    }
+    static double rate(){
+        return interestRate;
+    }
+    static void rate(double);
+private:
+    string owner;
+    double amount;
+    static double interestRate;
+    static double initRate();
+};
+
+void Account::rate(double newRate){
+    interestRate = newRate;
+}
+
+#endif
