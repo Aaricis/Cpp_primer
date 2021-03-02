d3423ee5ea93085fb14515e55b07fe8496263230
Aaricis <947165603@qq.com>
2021-03-02 18:25:43 +0800

    update

---
+++ Chapter16/16_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include"Sales_data.h"
+using namespace std;
+
+
+template<typename T>
+int compare(const T &v1, const T &v2){
+    if(v1 < v2) return -1;
+    if(v2 < v1) return 1;
+    return 0;
+}
+int main(){
+    //Sales_data a("123",10, 20), b("123",10, 30);
+    //cout<<compare(a, b)<<endl;
+    return 0;
+}
---
+++ Chapter16/Sales_data.h
@@ -0,0 +1,73 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Sales_data;
+istream& read(istream &is, Sales_data &sd);
+class Sales_data{
+friend Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
+friend istream &read(istream &is, Sales_data &sd);
+friend ostream &print(ostream &os, const Sales_data &sd);
+
+private:
+    string bookNo = "";
+    unsigned units_sold = {0};
+    double revenue{0.0};
+
+public:
+    //Sales_data() = default;
+    Sales_data():units_sold(0), revenue(0.0){}
+    Sales_data(const string &s): bookNo(s) {}
+    Sales_data(const string &s, unsigned n, double p): bookNo(s), units_sold(n), revenue(p*n){}
+    Sales_data(istream &is){
+        read(is, *this);
+    }
+
+    string isbn() const {return bookNo;}
+    Sales_data& combine(const Sales_data&);
+    double avg_price() const;
+};
+
+
+
+ostream& print(ostream &os, const Sales_data &sd);
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
+
+
+
+Sales_data& Sales_data::combine(const Sales_data &rhs){
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+
+inline double Sales_data::avg_price() const{
+    if(units_sold) 
+        return revenue/units_sold;
+    else 
+        return 0;
+}
+
+istream& read(istream &is, Sales_data &sd){
+    double price = 0.0;
+    is>>sd.bookNo>>sd.units_sold>>price;
+    sd.revenue = sd.units_sold*price;
+    return is;
+}
+
+ostream& print(ostream &os, const Sales_data &sd){
+    os<<sd.bookNo<<' '<<sd.units_sold<<' '<<sd.revenue;
+    return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs){
+    Sales_data sum;
+    sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+#endif
