e76f482efbd45f635c86e7c4ca4761d2b9d891ce
Aaricis <947165603@qq.com>
2021-03-09 22:00:35 +0800

    commit

---
+++ Chapter16/16_48.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+template <typename T> string debug_rep(const T &t){
+    ostringstream ret;
+    ret<<t;
+    return ret.str();
+}
+
+template <typename T> string debug_rep(T *p){
+    ostringstream ret;
+    ret<<"pointer:"<<p;
+    if(p)
+        ret<<" "<<debug_rep(*p);
+    else
+    {
+        ret<<" null pointers";
+    }
+    return ret.str();
+}
+
+string debug_rep(char *p){
+    return debug_rep(string(p));
+}
+
+string debug_rep(const char *p){
+    return debug_rep(string(p));
+}
+
+string debug_rep(const string &s){
+    return '"'+s+'"';
+} 
+
+int main(){
+    string s("hi");
+    cout<<debug_rep(s)<<endl;
+
+    const string *sp = &s;
+    cout<<debug_rep(sp)<<endl;
+
+    cout << debug_rep("hi world!") <<endl;
+    return 0;
+}
